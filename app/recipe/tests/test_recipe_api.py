d35296de679a7f3bd342862d3f62af70fe4c5945
Marcio Woitek <mwoitekjr@gmail.com>
2023-03-12 16:46:26 -0300

    Add tests for creating a recipe

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -105,3 +105,20 @@ class PrivateRecipeApiTests(TestCase):
             res.data,  # pyright: ignore
             serializer.data,
         )
+
+    def test_create_recipe(self):
+        """Test creating a recipe."""
+        payload = {
+            "title": "Sample recipe",
+            "time_minutes": 30,
+            "price": Decimal("5.99"),
+        }
+        res = self.client.post(RECIPES_URL, data=payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        recipe = Recipe.objects.get(
+            id=res.data["id"],  # pyright: ignore
+        )
+        for k, v in payload.items():
+            self.assertEqual(getattr(recipe, k), v)
+        self.assertEqual(recipe.user, self.user)
