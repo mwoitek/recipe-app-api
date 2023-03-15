bc43e7a39a2677e6d7785a0352f7ad9b9d3b0433
Marcio Woitek <mwoitekjr@gmail.com>
2023-03-15 18:08:15 -0300

    Implement create ingredients feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -30,6 +30,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
@@ -40,6 +41,7 @@ class RecipeSerializer(serializers.ModelSerializer):
             "price",
             "link",
             "tags",
+            "ingredients",
         ]
         read_only_fields = ["id"]
 
@@ -50,11 +52,22 @@ class RecipeSerializer(serializers.ModelSerializer):
             tag_obj, _ = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed."""
+        auth_user = self.context["request"].user
+        for ingredient in ingredients:
+            ingredient_obj, _ = Ingredient.objects.get_or_create(
+                user=auth_user, **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop("tags", [])
+        ingredients = validated_data.pop("ingredients", [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
         return recipe
 
     def update(self, instance, validated_data):
